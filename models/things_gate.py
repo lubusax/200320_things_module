89134991c498a05727585f8a48f4346a22aff5df
lubusax <lu.bu.sax@gmail.com>
2020-03-21 20:45:46 +0100

    autosave

--- models/things_gate.py
+++ models/things_gate.py
@@ -1,4 +1,5 @@
 from odoo import models, fields
+from uuid import uuid4
 
 class ThingsGate(models.Model):
     _name = 'things.gate'
@@ -6,7 +7,21 @@ class ThingsGate(models.Model):
     name = fields.Char('Gate Name')
     location = fields.Char('Gate Location')
     route_to_gate =fields.Char(
-        'route for outgoing data from the database to the gate')
+        string = 'route to gate',
+        help = 'route for outgoing data from the database to the gate',
+        default = lambda self: (
+            str(fields.Datetime.now())+str(uuid4())).
+            replace(" ","").
+            replace(":","").
+            replace("-","") ,
+        store = True,
+        compute_sudo = False,
+        readonly = True
+        )
     route_from_gate =fields.Char(
         'route for incoming data from the gate to the database')
+    
+    # def _compute_route_to_gate(self):
+    #     self.ensure_one()
+    #     self.route_to_gate = str(self.id.hex)+str(uuid.uuid4().hex) 
 
--- models/things_thing.py
+++ models/things_thing.py
@@ -1,4 +1,6 @@
 from odoo import models, fields
+from uuid import uuid4
+
 
 class ThingsThing(models.Model):
     _name = 'things.thing'
@@ -6,6 +8,24 @@ class ThingsThing(models.Model):
     # every thing sends and/or receives data
     # through one and only one gate
     gate_id = fields.Many2one('things.gate', string='Gate')
+    route_to_thing =fields.Char(
+        'route for outgoing data from the database to the thing',
+        default = lambda self: str(uuid4()),
+        store = True,
+        compute_sudo = False
+        )
+    route_from_thing =fields.Char(
+        'route for incoming data from the thing to the database')
+    can_receive = fields.Boolean(
+        'can process/needs data from the database')
+    can_send = fields.Boolean(
+        'can send data to the database')
+
+    # def _compute_route_to_thing(self):
+    #     self.ensure_one()
+    #     self.route_to_thing = str(self.id.hex)+str(uuid.uuid4().hex)
+
+    
 
 
 class ThingsGate(models.Model):
