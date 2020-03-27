f3e7267c850b14a5773815cb0a07af1b30481e88
lubusax <lu.bu.sax@gmail.com>
2020-03-27 15:35:38 +0100

    autosave - confirm gate

--- wizard/gate_confirm_wizard.py
+++ wizard/gate_confirm_wizard.py
@@ -1,21 +1,24 @@
 from odoo import models, fields
+from odoo.exceptions import UserError
 
 class GateConfirmWizard(models.TransientModel):
     _name = 'gate.confirm.wizard'
     _description = 'description'
     
-    gate_id = fields.Many2one(
-            'things.gate',
-            string = 'Gate to be Confirmed')
     name = fields.Char(string='Gate Name')
     location = fields.Char(string='Gate Location')
 
     def confirm_new_gate(self):
-        for wiz in self:
-            wiz.gate_id.write({
+        self.ensure_one()
+        gate = self.env['things.gate'].sudo().search(
+            [('name', '=', 'Awaiting to be Confirmed')])
+        if gate:
+            gate.write({
                 'registrationConfirmed' : True,
-                'name' : wiz.name,
-                'location' : wiz.location,
+                'name' : self.name,
+                'location' : self.location,
                 })
-        
+        else:
+            raise UserError('There is no Gate '
+                            'awaiting to be confirmed')
             
